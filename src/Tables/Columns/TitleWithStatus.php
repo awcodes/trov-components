02a498bfa1c780213816bbb599496b78cd61c39d
awcodes <adam@adamwgd.com>
2022-04-26 13:10:59 -0400

    tweak: styling and logic for title with slug component

--- src/Tables/Columns/TitleWithStatus.php
+++ src/Tables/Columns/TitleWithStatus.php
@@ -12,7 +12,11 @@ class TitleWithStatus extends TextColumn
     {
         $this->formatStateUsing(function ($record, $state) {
             if ($record->front_page) {
-                return new HtmlString($state . ' — <strong class="px-2 py-1 text-xs text-white bg-gray-900 rounded-md">Front Page</strong>');
+                return new HtmlString($state . ' — <strong class="px-2 py-1 text-xs text-white rounded-md bg-success-600">Front Page</strong>');
+            }
+
+            if ($record->deleted_at) {
+                return new HtmlString($state . ' — <strong class="px-2 py-1 text-xs text-white rounded-md bg-danger-600 dark:bg-danger-600/50">Trashed</strong>');
             }
 
             if ($record->status == Status::Draft->name) {
@@ -20,10 +24,10 @@ class TitleWithStatus extends TextColumn
             }
 
             if ($record->status == Status::Review->name) {
-                return new HtmlString($state . ' — <strong class="px-2 py-1 text-xs text-white rounded-md bg-warning-600 dark:bg-warning-600/50">Review</strong>');
+                return new HtmlString($state . ' — <strong class="px-2 py-1 text-xs text-white rounded-md bg-warning-600 dark:bg-warning-600/50">In Review</strong>');
             }
 
-            return $record->deleted_at ? new HtmlString($state . ' — <strong class="px-2 py-1 text-xs text-white rounded-md bg-danger-600 dark:bg-danger-600/50">Trashed</strong>') : $state;
+            return $state;
         });
     }
 }
