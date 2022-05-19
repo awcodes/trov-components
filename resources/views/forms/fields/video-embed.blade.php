655280575f882dbdcb1ab7e744f607048b2e1b51
awcodes <adam@adamwgd.com>
2022-05-19 14:40:36 -0400

    fine tune video embed component

--- resources/views/forms/fields/video-embed.blade.php
+++ resources/views/forms/fields/video-embed.blade.php
@@ -6,33 +6,37 @@
     :hint-icon="$getHintIcon()"
     :required="$isRequired()"
     :state-path="$getStatePath()">
-    <div @class([
-        'border rounded-lg',
-        'dark:border-gray-600 dark:text-white' => config('forms.dark_mode'),
-    ])>
+    <div>
         @if ($getState() != '')
-            <div class="p-4">
-                <h3 class="text-base">Preview</h3>
-                <div class="flex justify-center mt-2">
+            <div @class([
+                'p-4 relative',
+                'border-t border-x rounded-t-lg' => $getState(),
+                'dark:border-gray-600 dark:bg-transparent dark:text-white' => config(
+                    'forms.dark_mode'
+                ),
+                'border-gray-300 bg-gray-100' => !$errors->has($getStatePath()),
+                'border-danger-600 ring-danger-600' => $errors->has($getStatePath()),
+            ])>
+                <h3 @class([
+                    'absolute py-0.5 px-2 top-0 left-0 text-xs border-b border-r rounded-br',
+                    'dark:border-gray-600 dark:text-white' => config('forms.dark_mode'),
+                    'border-gray-300' => !$errors->has($getStatePath()),
+                ])>Preview</h3>
+                <div class="grid place-content-center">
                     {!! $getState() !!}
                 </div>
             </div>
         @endif
-        <textarea {!! ($autocapitalize = $getAutocapitalize()) ? "autocapitalize=\"{$autocapitalize}\"" : null !!}
-            {!! ($autocomplete = $getAutocomplete()) ? "autocomplete=\"{$autocomplete}\"" : null !!}
+        <textarea id="{{ $getId() }}"
             {!! $isAutofocused() ? 'autofocus' : null !!}
-            {!! ($cols = $getCols()) ? "cols=\"{$cols}\"" : null !!}
             {!! $isDisabled() ? 'disabled' : null !!}
-            id="{{ $getId() }}"
-            dusk="filament.forms.{{ $getStatePath() }}"
-            {!! filled($length = $getMaxLength()) ? "maxlength=\"{$length}\"" : null !!}
-            {!! filled($length = $getMinLength()) ? "minlength=\"{$length}\"" : null !!}
-            {!! ($placeholder = $getPlaceholder()) ? "placeholder=\"{$placeholder}\"" : null !!}
             {!! $isRequired() ? 'required' : null !!}
             {!! ($rows = $getRows()) ? "rows=\"{$rows}\"" : null !!}
             {{ $applyStateBindingModifiers('wire:model') }}="{{ $getStatePath() }}"
             {{ $attributes->merge($getExtraAttributes())->merge($getExtraInputAttributeBag()->getAttributes())->class([
-                    'block w-full transition duration-75 rounded-lg shadow-sm -m-px focus:border-primary-500 focus:ring-1 focus:ring-inset focus:ring-primary-600 filament-forms-textarea-component',
+                    'font-mono block w-full transition duration-75 shadow-sm focus:border-primary-500 focus:ring-1 focus:ring-inset focus:ring-primary-600 filament-forms-textarea-component',
+                    'rounded-lg' => !$getState(),
+                    'rounded-b-lg' => $getState(),
                     'dark:bg-gray-700 dark:border-gray-600 dark:text-white' => config('forms.dark_mode'),
                     'border-gray-300' => !$errors->has($getStatePath()),
                     'border-danger-600 ring-danger-600' => $errors->has($getStatePath()),
