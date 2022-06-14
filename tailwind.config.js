c1ff7c02d68ff8350b6de2dcfe40476639a77955
awcodes <adam@adamwgd.com>
2022-06-14 09:03:33 -0400

    remove preflight from tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-    content: ["./resources/views/**/*.blade.php", "./src/**/*.php"],
-    darkMode: "class",
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+  content: ["./resources/views/**/*.blade.php", "./src/**/*.php"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  corePlugins: {
+    preflight: false,
+  },
+  plugins: [],
 };
